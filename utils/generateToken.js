b5ea49ee1545c68b9451661f5a2b9619ccd3c846
unknown <tanaygandhi306@gmail.com>
2021-09-30 00:56:38 +0530

    final commit

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -4,8 +4,6 @@ const generateToken = () => {
   const token = jwt.sign({id: process.env.ID}, process.env.JWT_SECRET, {
     expiresIn: '24h',
   });
-  console.log('1');
-  console.log(token);
   return token
 };
 
